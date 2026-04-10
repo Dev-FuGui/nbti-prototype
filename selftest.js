dec76da7948c742f267deebf2fa1db571dd42237
DevFuGui <fuguidev@gmail.com>
2026-04-11 01:15:52 +0800

    feat: rewrite nbti question bank for production

--- selftest.js
+++ selftest.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 const vm = require('vm');
-let code = fs.readFileSync('app.js', 'utf8');
+const appPath = path.join(__dirname, 'app.js');
+let code = fs.readFileSync(appPath, 'utf8');
 code += '\nthis.__TEST__ = { TYPES, WEIGHTS, QUESTIONS };';
 const sandbox = {
   console,
@@ -15,6 +17,11 @@ const sandbox = {
 vm.createContext(sandbox);
 vm.runInContext(code, sandbox);
 const { TYPES, WEIGHTS, QUESTIONS } = sandbox.__TEST__;
+
+function assert(condition, message) {
+  if (!condition) throw new Error(message);
+}
+
 function calc(fill) {
   const scores = Object.keys(TYPES).reduce((acc, key) => {
     acc[key] = 0;
@@ -31,6 +38,19 @@ function calc(fill) {
     .map(([code, score]) => `${code}:${score}`)
     .join(', ');
 }
+assert(QUESTIONS.length === 24, `Expected 24 questions, got ${QUESTIONS.length}`);
+QUESTIONS.forEach((question, index) => {
+  assert(question.options.length === 3, `Question ${index + 1} should have 3 options`);
+  ['A', 'B', 'C'].forEach((key) => {
+    assert(WEIGHTS[index + 1] && WEIGHTS[index + 1][key], `Missing weights for Q${index + 1}${key}`);
+    Object.keys(WEIGHTS[index + 1][key]).forEach((code) => {
+      assert(TYPES[code], `Unknown type ${code} in Q${index + 1}${key}`);
+    });
+  });
+});
+
+console.log('question count =>', QUESTIONS.length);
 console.log('all A =>', calc('A'));
 console.log('all B =>', calc('B'));
 console.log('all C =>', calc('C'));
+console.log('selftest => ok');
